ead5d8a2267fcace3f3c2455d4d2e702e0e5ab29
samthetechie <samuelcarlisle@gmail.com>
2015-03-26 12:13:29 +0100

    cleaned up all old files

--- php/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-<p>Chez Madeleine | <a href="http://www.chezmadeleinebnb.com/">www.chezmadeleinebnb.com</a> | <a href="mailto:chezmadeleinebnb@gmail.com">chezmadeleinebnb@gmail.com</a> | 0033 553 79 78 67</p>
-</div>
--- php/navigation.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	<li><a href="./homepage">Home</a></li>
-	<li><a href="./maps">Maps</a></li>
-	<li><a href="./booking">Booking</a></li>
-	<li><a href="./contact">Contact</a></li>
-</ul>
